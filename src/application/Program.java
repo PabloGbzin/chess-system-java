75af84456ba3a62ee7558a1837d9c8757662f43b
Pablo GB <pablogb2027@gmail.com>
2024-01-20 17:49:54 -0300

    project created

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+import java.util.Scanner;
+import java.util.Locale;
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Hello world!");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
+
+	}
+
+}
