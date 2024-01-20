cd07733868fabf7f3d1e54bc403c8e362ebfef37
Pablo GB <pablogb2027@gmail.com>
2024-01-20 18:01:40 -0300

    frist class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 import java.util.Scanner;
+
+import boardgame.Position;
+
 import java.util.Locale;
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		Position position = new Position(1, 2);
 		
-		System.out.println("Hello world!");
+		System.out.println(position);
 		
 		
 		
